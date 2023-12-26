6034128f04146d3154c548b7825d08ae78efc960
Mahendra Reddy <122714066+mahe1999mh@users.noreply.github.com>
2023-12-26 22:50:25 +0530

    Update JS_LinkedList.js

--- linkedList/JS_LinkedList.js
+++ linkedList/JS_LinkedList.js
@@ -1,1 +1,24 @@
 
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Adding Data to Linked List
+const p1 = new Node(10);
+const p2 = new Node(20);
+const p3 = new Node(30);
+
+// Linking Addresses to Nodes
+p1.next = p2;
+p2.next = p3;
+const head = p1;
+
+// Printing Linked List
+let curr = head;
+while (curr !== null) {
+    console.log(" " + curr.data);
+    curr = curr.next;
+}
