f3181002ae38f613b73505fa283094841f08f0da
mahe1999mh <mahe1999mh@gmail.com>
2024-01-02 22:57:25 +0530

    add reveres linked list

--- linkedList/Day3_LinkedList.js
+++ linkedList/Day3_LinkedList.js
@@ -5,43 +5,69 @@ class Node {
   }
 }
 
-let head = null; // Initialize head as null
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-function add(data) {
-  const newNode = new Node(data);
-  if (head === null) {
-    head = newNode;
-  } else {
-    let curr = head;
-    while (curr.next !== null) {
+  add(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let curr = this.head;
+      while (curr.next !== null) {
+        curr = curr.next;
+      }
+      curr.next = newNode;
+    }
+  }
+
+  addFirst(data) {
+    const newNode = new Node(data);
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  reverse() {
+    let curr = this.head;
+    let prev = null;
+
+    while (curr != null) {
+      let next = curr.next;
+      curr.next = prev;
+
+      prev = curr;
+      curr = next;
+    }
+    this.head = prev;
+  }
+
+  printLinkedList() {
+    let curr = this.head;
+    while (curr !== null) {
+      console.log(curr.data);
       curr = curr.next;
     }
-    curr.next = newNode;
   }
 }
 
-function printLinkedList(head) {
-  let curr = head;
-  while (curr !== null) {
-    console.log(curr.data);
-    curr = curr.next;
-  }
-}
+const linkedList = new LinkedList();
 
-function addFist(data){
-    const newNode = new Node(data);
-    if(!head){
-        this.head = newNode;
-    }else{
-        newNode.next = head;
-        head = newNode
-    }
-}
+linkedList.add(10);
+linkedList.add(20);
+linkedList.add(30);
+linkedList.addFirst(5);
+linkedList.addFirst(1);
 
-add(10);
-add(20);
-add(30);
-addFist(5)
-addFist(1)
+console.log("\nOriginal Linked List:");
+linkedList.printLinkedList();
 
-printLinkedList(head);
+linkedList.reverse();
+
+console.log("\nReversed Linked List:");
+linkedList.printLinkedList();
