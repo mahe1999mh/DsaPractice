533bf61704aa485555250c8ffcb5ede82ccfddf4
maendra <mahendra.reddy@relyonsoft.com>
2024-10-24 15:44:04 +0530

    added binaryToDecimal

--- BitManipulation/decimalToBinary.js
+++ BitManipulation/decimalToBinary.js
@@ -9,5 +9,19 @@ function decimalToBinary(n) {
   return str;
 }
 
-decimalToBinary(45);
+function binaryToDecimal(str) {
+  let num = 0;
+  let power = 0;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] == "1") {
+      num += Math.pow(2, power);
+    }
+    power++;
+  }
+  return num;
+}
+
+let str = decimalToBinary(45);
 // output 101101
+console.log(binaryToDecimal(str));
+// output 45
