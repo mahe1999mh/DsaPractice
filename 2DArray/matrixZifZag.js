cc4cba71831f619f17cf9e2b12bdae326d2e90d5
maendra <mahendra.reddy@relyonsoft.com>
2024-10-25 22:59:42 +0530

    2d array ZifZag

---
+++ 2DArray/matrixZifZag.js
@@ -0,0 +1,18 @@
+let arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12, 13],
+  [14, 15, 16, 17],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  if (i / 2 == 0) {
+    for (let j = 0; j < arr.length; j++) {
+      console.log(arr[i][j]);
+    }
+  } else {
+    for (let j = arr.length - 1; j >= 0; j--) {
+      console.log(arr[i][j]);
+    }
+  }
+}
