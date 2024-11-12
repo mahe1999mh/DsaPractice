9c387b39a9e0a800c25ba7652f51459c95736221
mahe1999mh <mahe1999mh@gmail.com>
2024-11-12 23:12:13 +0530

    changes

---
+++ linkedList/removeNthFromEnd.js
@@ -0,0 +1,19 @@
+function removeNthFromEnd(head,n){
+  let slow = head;
+  let fast = head;
+
+  for (let i = 0; i < n; i++) {
+    if(!fast) return null
+   fast = fast.next;
+  }
+  if(!fast) return head.next;
+  
+  while(fast.next){
+    fast = fast.next
+    slow = slow.next
+  }
+
+  slow.next = slow.next.next;
+
+  return head
+}
